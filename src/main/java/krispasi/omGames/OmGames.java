3d22d615d4203d12460509dd1f64a417c4b6281d
Kryštof Holešta <123502609+Krispas@users.noreply.github.com>
2025-10-26 20:24:53 +0100

    idk

---
+++ src/main/java/krispasi/omGames/OmGames.java
@@ -0,0 +1,17 @@
+package krispasi.omGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OmGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
