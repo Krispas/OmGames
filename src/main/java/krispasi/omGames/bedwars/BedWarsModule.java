eb843c3406812ac305ac9c8eef0ac3915560fc1f
Kryštof Holešta <123502609+Krispas@users.noreply.github.com>
2025-11-21 22:00:10 +0100

    Register /bw command for Paper

--- src/main/java/krispasi/omGames/bedwars/BedWarsModule.java
+++ src/main/java/krispasi/omGames/bedwars/BedWarsModule.java
@@ -5,8 +5,13 @@ import krispasi.omGames.bedwars.config.BedWarsConfigService;
 import krispasi.omGames.bedwars.menu.BedWarsMenuFactory;
 import krispasi.omGames.bedwars.menu.BedWarsMenuListener;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class BedWarsModule {
     private final JavaPlugin plugin;
@@ -22,17 +27,8 @@ public class BedWarsModule {
 
         // command + listeners live here so we can wire everything simply
         BedWarsMenuFactory menuFactory = new BedWarsMenuFactory();
-        try {
-            BedWarsCommand commandHandler = new BedWarsCommand(menuFactory, configService);
-            if (plugin.getCommand("bw") != null) {
-                plugin.getCommand("bw").setExecutor(commandHandler);
-                plugin.getCommand("bw").setTabCompleter(commandHandler);
-            } else {
-                plugin.getLogger().warning("/bw command missing from paper-plugin.yml");
-            }
-        } catch (Exception ex) {
-            plugin.getLogger().warning("Could not wire BedWars command: " + ex.getMessage());
-        }
+        BedWarsCommand commandHandler = new BedWarsCommand(menuFactory, configService);
+        registerCommandSafe(commandHandler);
 
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(new BedWarsMenuListener(configService.getMaps(), configService.getDimensionName()), plugin);
@@ -44,5 +40,27 @@ public class BedWarsModule {
 
     public BedWarsConfigService getConfigService() {
         return configService;
+    }
+
+    private void registerCommandSafe(BedWarsCommand commandHandler) {
+        // Paper wants commands registered in code; fall back to Bukkit-style wiring if reflection fails
+        try {
+            Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, JavaPlugin.class);
+            constructor.setAccessible(true);
+            PluginCommand command = constructor.newInstance("bw", plugin);
+
+            command.setDescription("BedWars admin commands");
+            command.setUsage("/bw start");
+            command.setPermission("omgames.bw");
+            command.setPermissionMessage("You need omgames.bw to manage BedWars.");
+            command.setExecutor(commandHandler);
+            command.setTabCompleter(commandHandler);
+
+            Method getCommandMap = plugin.getServer().getClass().getMethod("getCommandMap");
+            CommandMap commandMap = (CommandMap) getCommandMap.invoke(plugin.getServer());
+            commandMap.register(plugin.getName().toLowerCase(), command);
+        } catch (Exception ex) {
+            plugin.getLogger().warning("Could not register /bw command: " + ex.getMessage());
+        }
     }
 }
